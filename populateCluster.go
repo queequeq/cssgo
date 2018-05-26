ea351b65c2519de380872e08a92a31189f7e7533
Julian <eddie_64@web.de>
2018-05-26 15:59:29 +0200

    Updated comments

--- populateCluster.go
+++ populateCluster.go
@@ -17,51 +17,70 @@ func main() {
 	fmt.Scanln(&ip)
 	// Überprüfen, ob eine IP-Adresse eingegeben wurde
 	if net.ParseIP(ip) == nil {
-		fmt.Println("Fehler: Keine gültige IP-Adresse!")
+		fmt.Println("Fehler: Keine gültige IP-Adresse")
 		return
 	}
 
-	fmt.Println("Wie viele Einträge sollen erstellt werden? (Gültiger Bereich: 0 bis 100000)")
+	fmt.Println("Wie viele Einträge sollen erstellt werden? (Gültiger Bereich: 0 bis 1000000)")
 	fmt.Scanln(&input)
 	count, err := strconv.Atoi(input)
 	// Überprüfen, ob eine Zahl eingegeben wurde und ob diese im zulässigen Wertebereich liegt
-	if err != nil || count < 0 || count > 100000 {
-		fmt.Println("Fehler: Ungültiger Wert!")
+	if err != nil || count < 0 || count > 1000000 {
+		fmt.Println("Fehler: Ungültiger Wert")
 		return
 	}
 
 	fmt.Println("Erstelle " + strconv.Itoa(count) + " Einträge...")
-	start := time.Now()
 	populateCluster(ip, count)
-	elapsed := time.Since(start)
-	fmt.Println(strconv.Itoa(count) + " Einträge erstellt in " + elapsed.String())
 }
 
+// Stellt eine Verbindung zum Cluster mit der angegebenen IP-Adresse her und fügt die angegebene Anzahl an Einträgen ein
 func populateCluster(ip string, count int) {
+	// Starten des Timers durch Ausführen des Arguments. Beim Verlassen der umgebenden Funktion wird dieser automatisch angehalten
+	defer timer(time.Now())
+
+	// Cluster-Konfiguration erstellen, Verbindung aufbauen und diese überprüfen
 	cluster := gocql.NewCluster(ip)
 	cluster.Keyspace = "data"
 	session, err := cluster.CreateSession()
-
 	if err != nil {
-		fmt.Println("Fehler: Verbindung konnte nicht hergestellt werden!")
 		fmt.Println(err)
 		return
 	}
 
+	// Verbindung beenden, sobald die Funktion verlassen wird
+	defer session.Close()
+
+	// Tabelle in der Datenbank erstellen, falls diese noch nicht vorhanden ist. Abbrechen, falls beim Erstellen ein Fehler auftritt
 	stmt := session.Query("CREATE TABLE IF NOT EXISTS cpuStats (timestamp timestamp PRIMARY KEY, temperature float, frequency int);")
 	stmt.Exec()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
+	// Vom User angegebene Anzahl ein Einträgen erzeugen und in die Datenbank einfügen
 	for i := 0; i < count; i++ {
+		// Channels vom Typ String erzeugen
 		tempChan := make(chan string)
 		freqChan := make(chan string)
+
+		// Goroutinen starten und Channels übergeben
 		go cpuTemp(tempChan)
 		go cpuFreq(freqChan)
+
+		// Werte aus den Channels auslesen und Eintrag in die Datenbank schreiben. Abbrechen, falls ein Fehler auftritt
 		stmt = session.Query("INSERT INTO cpuStats (timestamp, temperature, frequency) VALUES (toTimestamp(now()), " + <-tempChan + ", " + <-freqChan + ");")
 		err := stmt.Exec()
 		if err != nil {
 			fmt.Println(err)
+			return
 		}
 	}
+}
 
-	session.Close()
+// Gibt die Differenz zwischen dem übergebenen Zeitpunkt und dem aktuellen Zeitpunkt aus
+func timer(start time.Time) {
+	elapsed := time.Since(start)
+	fmt.Println("Dauer: " + elapsed.String())
 }
--- sensors.go
+++ sensors.go
@@ -8,32 +8,36 @@ import (
 
 // Liest die CPU-Taktfrequenz des Raspberry Pi aus und gibt diese als String zurück
 func cpuFreq(freqChan chan string) {
+	// Kommando auf dem Raspberry Pi ausführen und 0 in den Channel schreiben, falls ein Fehler auftritt
 	cmd := exec.Command("vcgencmd", "measure_clock", "arm")
 	out, err := cmd.Output()
-
 	if err != nil {
 		fmt.Println(err)
 		freqChan <- "0"
+		return
 	}
 
+	// Rückgabewert in String umwandeln, alle nicht benötigten Zeichen entfernen und Wert in den Channel ausgeben
 	freq := string(out)
-	freq = strings.TrimSpace(freq)              // Zeilenumbruch im Rückgabewert entfernen
-	freq = strings.Trim(freq, "frequency(45)=") // Nicht benötigte Zeichen im Rückgabewert entfernen
+	freq = strings.TrimSpace(freq)
+	freq = strings.Trim(freq, "frequency(45)=")
 	freqChan <- freq
 }
 
 // Liest die CPU-Temperatur des Raspberry Pi aus und gibt diese als String zurück
 func cpuTemp(tempChan chan string) {
+	// Kommando auf dem Raspberry Pi ausführen und 0 in den Channel schreiben, falls ein Fehler auftritt
 	cmd := exec.Command("vcgencmd", "measure_temp") // CPU-Temperatur auslesen
 	out, err := cmd.Output()
-
 	if err != nil {
 		fmt.Println(err)
 		tempChan <- "0.0"
+		return
 	}
 
+	// Rückgabewert in String umwandeln, alle nicht benötigten Zeichen entfernen und Wert in den Channel ausgeben
 	temp := string(out)
-	temp = strings.TrimSpace(temp)       // Zeilenumbruch im Rückgabewert entfernen
-	temp = strings.Trim(temp, "temp='C") // Nicht benötigte Zeichen im Rückgabewert entfernen
+	temp = strings.TrimSpace(temp)
+	temp = strings.Trim(temp, "temp='C")
 	tempChan <- temp
 }
