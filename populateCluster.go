3248363f6eecc0605127b787acc174eaeb73cf34
Julian <eddie_64@web.de>
2018-06-04 17:01:19 +0200

    Added flags and more concurrency

--- populateCluster.go
+++ populateCluster.go
@@ -1,32 +1,47 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 	"strconv"
+	"sync"
 	"time"
 
 	"github.com/gocql/gocql"
 )
 
 func main() {
-	var ip string
-	var input string
+	//var ip string
+	//var input string
 
-	fmt.Println("Bitte IP einer der Nodes im Cluster angeben:")
-	fmt.Scanln(&ip)
+	ipPtr := flag.String("ip", "", "IP-Adresse einer der Nodes im Cluster")
+	countPtr := flag.Int("n", 0, "Anzahl der Einträge, die eingefügt werden sollen")
+	flag.Parse()
+	ip := *ipPtr
+	count := *countPtr
+	fmt.Println("Eingegebene IP:", ip)
+	fmt.Println("Anzahl:", count)
+
+	//fmt.Println("Bitte IP einer der Nodes im Cluster angeben:")
+	//fmt.Scanln(&ip)
 	// Überprüfen, ob eine IP-Adresse eingegeben wurde
 	if net.ParseIP(ip) == nil {
-		fmt.Println("Fehler: Keine gültige IP-Adresse")
+		flag.PrintDefaults()
 		return
 	}
 
-	fmt.Println("Wie viele Einträge sollen erstellt werden? (Gültiger Bereich: 0 bis 10000000)")
-	fmt.Scanln(&input)
-	count, err := strconv.Atoi(input)
+	//fmt.Println("Wie viele Einträge sollen erstellt werden? (Gültiger Bereich: 0 bis 10000000)")
+	//fmt.Scanln(&input)
+	//count, err := strconv.Atoi(input)
 	// Überprüfen, ob eine Zahl eingegeben wurde und ob diese im zulässigen Wertebereich liegt
-	if err != nil || count < 0 || count > 10000000 {
+	/*if err != nil || count < 0 || count > 10000000 {
 		fmt.Println("Fehler: Ungültiger Wert")
+		return
+	}*/
+
+	if count < 0 || count > 10000000 {
+		flag.PrintDefaults()
 		return
 	}
 
@@ -59,6 +74,10 @@ func populateCluster(ip string, count int) {
 		return
 	}
 
+	var wg sync.WaitGroup
+	wg.Add(count)
+	limit := make(chan int, 10)
+
 	// Vom User angegebene Anzahl ein Einträgen erzeugen und in die Datenbank einfügen
 	for i := 0; i < count; i++ {
 		// Channels vom Typ String erzeugen
@@ -69,14 +88,26 @@ func populateCluster(ip string, count int) {
 		go cpuTemp(tempChan)
 		go cpuFreq(freqChan)
 
+		temp := <-tempChan
+		freq := <-freqChan
+
 		// Werte aus den Channels auslesen und Eintrag in die Datenbank schreiben. Abbrechen, falls ein Fehler auftritt
-		stmt = session.Query("INSERT INTO cpuStats (timestamp, temperature, frequency) VALUES (toTimestamp(now()), " + <-tempChan + ", " + <-freqChan + ");")
-		err := stmt.Exec()
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
+		go func(session *gocql.Session, temp string, freq string) {
+			defer wg.Done()
+			limit <- 1
+
+			stmt = session.Query("INSERT INTO cpuStats (timestamp, temperature, frequency) VALUES (toTimestamp(now()), " + temp + ", " + freq + ");")
+			err := stmt.Exec()
+			if err != nil {
+				fmt.Println(err)
+				//os.Exit(1)
+			}
+
+			<-limit
+		}(session, temp, freq)
 	}
+
+	wg.Wait()
 }
 
 // Gibt die Differenz zwischen dem übergebenen Zeitpunkt und dem aktuellen Zeitpunkt aus
